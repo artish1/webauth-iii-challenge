6814ac068876a6c989e65781b6b523278e62af0e
Mark Artishuk <markyshuk@gmail.com>
2019-12-18 12:09:41 -0800

    Created routes but no functionality to them yet

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const apiRouter = require("./routes/apiRouter");
 const server = express();
 
 server.use(express.json());
+server.use("/api", apiRouter);
 
 module.exports = server;
