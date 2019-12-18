410edab3e569385f2c74e7d53a3f0722f887ed7f
Mark Artishuk <markyshuk@gmail.com>
2019-12-18 12:27:44 -0800

    Finished functionality for register route

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,8 +1,41 @@
 const express = require("express");
-
+const Users = require("../models/user-models");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  const body = req.body;
+
+  if (!body.username || !body.password || !body.department) {
+    res.status(400).json({
+      message: "username, password, and department fields are required"
+    });
+  } else {
+    bcrypt.hashSync(body.password, 8, (err, hash) => {
+      if (err) {
+        console.log("Error hashing: ", err);
+        res.status(500).json({
+          message: "There was a server error trying to hash the password"
+        });
+      } else {
+        const user = { ...body, password: hash };
+        Users.add(user)
+          .then(newUser => {
+            res.status(201).json(newUser);
+          })
+          .catch(err => {
+            console.log(err);
+            res
+              .status(500)
+              .json({
+                message: "There was an error adding the user to the db"
+              });
+          });
+      }
+    });
+  }
+});
 
 router.post("/login", (req, res) => {});
 
